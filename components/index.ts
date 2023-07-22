2c82246c09e86d38d7bba0aec36a2016521712d2
NahuelFD <nahuelfd10@gmail.com>
2023-07-22 16:14:24 -0300

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  CarCard,
+  SearchManufacturer,
+  CarDetails,
+  ShowMore,
+};
